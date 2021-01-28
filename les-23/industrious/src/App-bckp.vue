aff4feb3f467b64c3d0813798c316f2325907099
Korneel Eeckhout <korneel@pienter.tech>
2021-01-28 20:54:55 +0100

    components:  - Highlight.vue

--- les-23/industrious/src/App-bckp.vue
+++ les-23/industrious/src/App-bckp.vue
@@ -1,3 +1,4 @@
+n
 <template>
   <Header />
   <!-- Banner -->
---
+++ les-23/industrious/src/components/Highlight.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+    <div class="content">
+      <header>
+        <a href="#" :class="['icon', iconClass]"
+          ><span class="label">Icon</span></a
+        >
+        <h3>{{ title }}</h3>
+      </header>
+      <p>
+        {{ text }}
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Highlight',
+  props: {
+    iconClass: {
+      type: String,
+      default: 'fa-vcard-o',
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
